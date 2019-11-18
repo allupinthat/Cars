b93de4373625da073870744793725b0650e24d21
allupinthat <52768065+allupinthat@users.noreply.github.com>
2019-11-17 23:23:48 -0500

    Selection for Anubis

--- police/data/vehicle_names.lua
+++
@@ -1,3 +0,0 @@
-Citizen.CreateThread(function()
-	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), "0x79FBB0C5", "LSPD Ford Crown Vic")
-end)
--- police4/data/vehicle_names.lua
+++
@@ -1,3 +0,0 @@
-Citizen.CreateThread(function()
-    Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), "police4", "LSPD Chevy Impala")
-end)
